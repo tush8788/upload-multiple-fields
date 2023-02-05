28dbdbc5a507f5973f0a895b7f9305fd99aac5b5
tush87 <tusharj8788@gmail.com>
2023-02-05 21:03:27 +0530

    add form to upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const expressLayout=require('express-ejs-layouts');
+const port=8000;
+
+const app=express();
+
+app.set('view engine','ejs');
+
+app.set('views','./views');
+
+app.use(expressLayout);
+
+app.use('/',require('./routes/index.js'));
+
+app.listen(port,function(err){
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(`server is up on port ${port}`);
+})
